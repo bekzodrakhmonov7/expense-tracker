5b33d300c1347c50c85a792bdfd6614a3af102bc
bekzodrakhmonov7 <bekzodrakhmonov7@gmail.com>
2026-02-22 14:42:10 +0200

    added expense edit functionality
    fixed typo expence -> expense.

--- main.py
+++ main.py
@@ -3,49 +3,50 @@ import json
 from uuid import uuid4
 import datetime
 
-FILE = "expences.json"
+FILE = "expenses.json"
 
 
-def get_curr_expences():
+def get_curr_expenses():
     with open(file=FILE, mode="r") as file:
-        expences = json.load(file)
-    return expences
+        expenses = json.load(file)
+    return expenses
 
 
-def add_expence(args):
+def add_expense(args):
     id = uuid4()
-    expences = get_curr_expences()
-    new_expence = {
+    expenses = get_curr_expenses()
+    new_expense = {
         "id": str(id),
         "description": args.description,
         "amount": args.amount,
         "createdAt": datetime.datetime.now(datetime.timezone.utc).isoformat(
             timespec="seconds"
         ),
+        "updatedAt": None,
     }
-    expences.append(new_expence)
+    expenses.append(new_expense)
     with open(file=FILE, mode="w") as file:
-        json.dump(expences, file, indent=4)
+        json.dump(expenses, file, indent=4)
     print(f"Expense added successfully (ID: {id})")
 
 
-def list_expences():
-    expences = get_curr_expences()
-    print(json.dumps(expences, indent=4))
+def list_expenses():
+    expenses = get_curr_expenses()
+    print(json.dumps(expenses, indent=4))
 
 
-def remove_expence(args):
-    expences = get_curr_expences()
-    new_expences = []
-    for expence in expences:
-        if expence["id"] != args.id:
-            new_expences.append(expence)
-    if expences == new_expences:
-        print(f"The expence with id {args.id} does not exist")
+def remove_expense(args):
+    expenses = get_curr_expenses()
+    new_expenses = []
+    for expense in expenses:
+        if expense["id"] != args.id:
+            new_expenses.append(expense)
+    if expenses == new_expenses:
+        print(f"The expense with id {args.id} does not exist")
     else:
         with open(file=FILE, mode="w") as file:
-            json.dump(new_expences, file, indent=4)
-        print("Expence was successfully removed")
+            json.dump(new_expenses, file, indent=4)
+        print("expense was successfully removed")
 
 
 MONTH_MAPPING = [
@@ -64,63 +65,90 @@ MONTH_MAPPING = [
 ]
 
 
-def summarize_expences(args, months=MONTH_MAPPING):
-    expences = get_curr_expences()
+def summarize_expenses(args, months=MONTH_MAPPING):
+    expenses = get_curr_expenses()
     total_amount = 0
 
     if args.month is None:
-        for expence in expences:
-            total_amount += expence["amount"]
-        print(f"Total expences: ${total_amount}")
+        for expense in expenses:
+            total_amount += expense["amount"]
+        print(f"Total expenses: ${total_amount}")
     else:
         if 12 < args.month < 0:
             print("Specified month exceeds the number of month in a year (1-12)")
             return
-        for expence in expences:
-            date = datetime.datetime.fromisoformat(expence["createdAt"])
+        for expense in expenses:
+            date = datetime.datetime.fromisoformat(expense["createdAt"])
             if date.month == args.month:
-                total_amount += expence["amount"]
+                total_amount += expense["amount"]
         print(f"Total amount spent in {months[args.month - 1]} is {total_amount}")
+
+
+def edit_expense(args):
+    expenses = get_curr_expenses()
+    for expense in expenses:
+        if expense["id"] == args.id:
+            expense["description"] = args.description
+            expense["updatedAt"] = datetime.datetime.now(
+                datetime.timezone.utc
+            ).isoformat(timespec="seconds")
+    with open(file=FILE, mode="w") as file:
+        json.dump(expenses, file, indent=4)
+    print("Expense was successfully edited")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="Expence Tracker", description="CLI tool to track your expences"
+        prog="expense Tracker", description="CLI tool to track your expenses"
     )
     subparsers = parser.add_subparsers(dest="command")
 
-    add_subparser = subparsers.add_parser("add", help="Add expences")
-    list_subparser = subparsers.add_parser("list", help="List expences")
-    remove_subparser = subparsers.add_parser("remove", help="Remove expences")
+    add_subparser = subparsers.add_parser("add", help="Add expenses")
+    list_subparser = subparsers.add_parser("list", help="List expenses")
+    remove_subparser = subparsers.add_parser("remove", help="Remove expenses")
     summary_subparser = subparsers.add_parser(
-        "summary", help="Total amount of expences tacked"
+        "summary", help="Total amount of expenses tacked"
     )
+    edit_subparser = subparsers.add_parser("edit", help="Edit existing expense")
 
     add_subparser.add_argument(
-        "-d", "--description", type=str, help="Description of expence", required=True
+        "-d", "--description", type=str, help="Description of expense", required=True
     )
     add_subparser.add_argument(
         "-a", "--amount", type=int, help="Amount spent", required=True
     )
 
     remove_subparser.add_argument(
-        "--id", type=str, help="ID of an expence to remove", required=True
+        "--id", type=str, help="ID of an expense to remove", required=True
     )
 
     summary_subparser.add_argument(
         "-m", "--month", type=int, help="Month to specify", required=False
     )
 
+    edit_subparser.add_argument(
+        "--id", type=str, help="ID of expense to edit", required=True
+    )
+    edit_subparser.add_argument(
+        "-d",
+        "--description",
+        type=str,
+        help="New description of expense",
+        required=True,
+    )
+
     args = parser.parse_args()
 
     if args.command == "add":
-        add_expence(args=args)
+        add_expense(args=args)
     elif args.command == "list":
-        list_expences()
+        list_expenses()
     elif args.command == "remove":
-        remove_expence(args=args)
+        remove_expense(args=args)
     elif args.command == "summary":
-        summarize_expences(args)
+        summarize_expenses(args)
+    elif args.command == "edit":
+        edit_expense(args)
 
 
 if __name__ == "__main__":
